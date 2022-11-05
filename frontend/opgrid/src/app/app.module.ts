fe7dbbf8fc76f37c688ce47c32e4d2e04fc7b46a
trachanh1609 <trachanh@gmail.com>
2022-11-05 14:24:49 +0200

    init map

--- frontend/opgrid/src/app/app.module.ts
+++ frontend/opgrid/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatButtonModule} from '@angular/material/button';
+import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
 
 import { DistributionComponent } from './distribution/distribution.component';
 
 @NgModule({
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   declarations: [
     AppComponent,
     DistributionComponent
@@ -20,6 +24,7 @@ import { DistributionComponent } from './distribution/distribution.component';
     BrowserAnimationsModule,
     MatTabsModule,
     MatButtonModule,
+    NgxMapLibreGLModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
